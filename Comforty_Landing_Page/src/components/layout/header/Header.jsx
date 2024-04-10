35860609fefb6b58ee68e8b909ce80a2238f6bf2
Anna <protsenko.anyta@gmail.com>
2024-04-10 13:32:57 +0200

    added elements to the middle nav bar

--- Comforty_Landing_Page/src/components/layout/header/Header.jsx
+++ Comforty_Landing_Page/src/components/layout/header/Header.jsx
@@ -1,10 +1,12 @@
 import "./Header.css";
 import Topline from "./topline/Topline";
+import NavBar from "./navigation/NavBar";
 
 function Header() {
   return (
     <div className="header">
       <Topline />
+      <NavBar />
     </div>
   );
 }
--- Comforty_Landing_Page/src/components/layout/header/navigation/NavBar.jsx
+++ Comforty_Landing_Page/src/components/layout/header/navigation/NavBar.jsx
@@ -1,5 +1,28 @@
+import "./NavBar.css";
+import logoImg from "../../../../assets/images/Logo.svg";
+import searchIcon from "../../../../assets/images/search.svg";
+
 function NavBar() {
-  return <div></div>;
+  return (
+    <div className="nav-bar">
+      <div className="main-nav-bar">
+        <img src={logoImg} alt="Logo" />
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search here..."
+        />
+        <button type="submit" className="search-button">
+          <img className="search-icon" src={searchIcon} alt="searchIcon" />
+        </button>
+        <div className="user-buttons">
+          <button></button>
+          <button></button>
+          <button></button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavBar;
